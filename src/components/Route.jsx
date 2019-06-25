e63ea31b75e050a8e0bf5f792e1c182cc5d6b91f
calmgust <calmgust@gmail.com>
2019-06-25 13:27:44 +0900

    create about page

--- src/components/Route.jsx
+++ src/components/Route.jsx
@@ -2,22 +2,21 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import MainPage from "../pages/MainPage";
+import About from "../pages/About";
 import SendMail from "../pages/SendMail";
 import TestMail from "../pages/TestMail";
 import SendLater from "../pages/SendLater";
 import UserSetting from "../pages/UserSetting";
 
-import TempUserSetting from "../pages/TempUserSetting";
-
 const MainRoute = props => {
   return (
     <>
       <Route exact path="/" component={MainPage} />
+      <Route path="/about" component={About} />
       <Route path="/sendmail" component={SendMail} />
       <Route path="/testmail" component={TestMail} />
       <Route path="/sendlater" component={SendLater} />
       <Route path="/usersetting" component={UserSetting} />
-      <Route path="/tempusersetting" component={TempUserSetting} />
     </>
   );
 };
---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+import { PageWrapper } from "../styles/PageWrapper";
+
+const About = () => {
+  return <AboutWrapper>About Page</AboutWrapper>;
+};
+
+const AboutWrapper = styled(PageWrapper)``;
+
+export default About;
