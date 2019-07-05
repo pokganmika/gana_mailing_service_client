1bebddeeceb8aa071e867cd69d8db895a5cb9866
pokganmika <calmgust@gmail.com>
2019-07-05 17:06:20 +0900

    modify bool value -> num value in user setting page

--- src/pages/UserSetting.jsx
+++ src/pages/UserSetting.jsx
@@ -18,88 +18,69 @@ export default function MaterialTableDemo() {
         field: "created_at",
       },
     ],
-    data: [
-      // {
-      //   email: "gana@gananetworks.com",
-      //   type: "email",
-      //   subscribed: true,
-      //   created_at: "2019-06-20",
-      // },
-      // {
-      //   email: "ganagana@gmail.com",
-      //   type: "email",
-      //   subscribed: false,
-      //   created_at: "2019-05-05",
-      // },
-    ],
-    // columns: [
-    //   { title: "Name", field: "name" },
-    //   { title: "Surname", field: "surname" },
-    //   { title: "Birth Year", field: "birthYear", type: "numeric" },
-    //   {
-    //     title: "Birth Place",
-    //     field: "birthCity",
-    //     lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
-    //   },
-    // ],
-    // data: [
-    //   { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 },
-    //   {
-    //     name: "Zerya Betül",
-    //     surname: "Baran",
-    //     birthYear: 2017,
-    //     birthCity: 34,
-    //   },
-    // ],
+    data: [],
   });
-
-  // const target = "http://192.168.0.114";
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios.get(`${SERVER_URL}/subscribe`);
-      // const result = await axios.get("http://192.168.0.114/subscribe");
       // .then(res => console.log(res))
       // .catch(err => console.log(err));
-      console.log("fetchData : ", result);
-      console.log("fetchData Arr : ", result.data);
       setState({ ...state, data: result.data });
     };
     fetchData();
   }, []);
 
-  // TODO: data.created_at => unix time modify
+  /**
+   * TODO: DB issue
+   * data.created_at => unix time check
+   * data.subscrived => bool value -> num value
+   */
   const addData = async data => {
     console.log("UserSetting::addData::check -> ", data);
-    if (!data.email) {
+    // if (!data.email) {
+    // } else {
+    //   if (!data.type || !data.created_at) {
+    //     if (!data.type) {
+    //       data.type = "email";
+    //     }
+    //     if (!data.created_at) {
+    //       // data.created_at = moment()
+    //       //   .format()
+    //       //   .slice(0, 10);
+    //       data.created_at = new Date().getTime();
+    //     }
+    //     await axios.post(`${SERVER_URL}/subscribe/add`, data);
+    //   } else {
+    //     await axios.post(`${SERVER_URL}/subscribe/add`, data);
+    //   }
+    // }
+    data.type = "email";
+    data.created_at = new Date().getTime();
+    if (data.subscribed === true) {
+      data.subscribed = 1;
     } else {
-      if (!data.type || !data.created_at) {
-        if (!data.type) {
-          data.type = "email";
-        }
-        if (!data.created_at) {
-          data.created_at = moment()
-            .format()
-            .slice(0, 10);
-        }
-        await axios.post(`${SERVER_URL}/subscribe/add`, data);
-      } else {
-        await axios.post(`${SERVER_URL}/subscribe/add`, data);
-      }
+      data.subscribed = 0;
     }
+    await axios.post(`${SERVER_URL}/subscribe/add`, data);
   };
 
   const deleteData = async data => {
-    console.log("UserSetting::deleteData::check -> ", data);
+    console.log("::UserSetting::deleteData::check -> ", data);
     await axios.post(`${SERVER_URL}/subscribe/delete`, data);
   };
 
   const updateData = async data => {
-    console.log("UserSetting::updateData::check -> ", data);
+    console.log("::UserSetting::updateData::check -> ", data);
+    if (data.subscribed === true) {
+      data.subscribed = 1;
+    } else {
+      data.subscribed = 0;
+    }
     await axios.post(`${SERVER_URL}/subscribe/update`, data);
   };
 
-  console.log("UserSetting::data:: -> ", state.data);
+  console.log("::UserSetting::data:: -> ", state.data);
   return (
     <UserSettingWrapper>
       <MaterialTable
