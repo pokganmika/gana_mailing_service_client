2f8a789c8fa403351a7ca427c4e1b23b9662d256
pokganmika <calmgust@gmail.com>
2019-07-17 14:19:05 +0900

    Update ui complete main page

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -149,37 +149,67 @@ const MainPage = props => {
 
               <div className="mail-status-card">
                 <ACard title="DELIVERED" bordered={false}>
-                  <p>{state.mailWeeklyData.delivered}</p>
+                  <p>
+                    {dataPerCalc(
+                      state.mailWeeklyData.delivered,
+                      state.mailWeeklyData.requests,
+                    )}
+                  </p>
                 </ACard>
               </div>
 
               <div className="mail-status-card">
                 <ACard title="OPENED" bordered={false}>
-                  <p>{state.mailWeeklyData.opens}</p>
+                  <p>
+                    {dataPerCalc(
+                      state.mailWeeklyData.opens,
+                      state.mailWeeklyData.requests,
+                    )}
+                  </p>
                 </ACard>
               </div>
 
               <div className="mail-status-card">
                 <ACard title="CLICKED" bordered={false}>
-                  <p>{state.mailWeeklyData.clicks}</p>
+                  <p>
+                    {dataPerCalc(
+                      state.mailWeeklyData.clicks,
+                      state.mailWeeklyData.requests,
+                    )}
+                  </p>
                 </ACard>
               </div>
 
               <div className="mail-status-card">
                 <ACard title="BOUNCES" bordered={false}>
-                  <p>{state.mailWeeklyData.bounces}</p>
+                  <p>
+                    {dataPerCalc(
+                      state.mailWeeklyData.bounces,
+                      state.mailWeeklyData.requests,
+                    )}
+                  </p>
                 </ACard>
               </div>
 
               <div className="mail-status-card">
                 <ACard title="SPAM REPORTS" bordered={false}>
-                  <p>{state.mailWeeklyData.spam_reports}</p>
+                  <p>
+                    {dataPerCalc(
+                      state.mailWeeklyData.spam_reports,
+                      state.mailWeeklyData.requests,
+                    )}
+                  </p>
                 </ACard>
               </div>
 
               <div className="mail-status-card">
                 <ACard title="UNSUBSCRIBED" bordered={false}>
-                  <p>{state.mailWeeklyData.unsubscribes}</p>
+                  <p>
+                    {dataPerCalc(
+                      state.mailWeeklyData.unsubscribes,
+                      state.mailWeeklyData.requests,
+                    )}
+                  </p>
                 </ACard>
               </div>
             </div>
@@ -194,63 +224,6 @@ const MainPage = props => {
               )}
             </>
           )}
-          {/* {state.mailWeeklyData ? (
-            <div className="mail-data">
-              <Card title={"REQUESTS"} data={state.mailWeeklyData.requests} />
-              <Card
-                title={"DELIVERED"}
-                data={state.mailWeeklyData.delivered}
-                per={
-                  state.mailWeeklyData.delivered / state.mailWeeklyData.requests
-                }
-              />
-              <Card
-                title={"OPENED"}
-                data={state.mailWeeklyData.opens}
-                per={state.mailWeeklyData.opens / state.mailWeeklyData.requests}
-              />
-              <Card
-                title={"CLICKED"}
-                data={state.mailWeeklyData.clicks}
-                per={
-                  state.mailWeeklyData.clicks / state.mailWeeklyData.requests
-                }
-              />
-              <Card
-                title={"BOUNCES"}
-                data={state.mailWeeklyData.bounces}
-                per={
-                  state.mailWeeklyData.bounces / state.mailWeeklyData.requests
-                }
-              />
-              <Card
-                title={"SPAM REPORTS"}
-                data={state.mailWeeklyData.spam_reports}
-                per={
-                  state.mailWeeklyData.spam_reports /
-                  state.mailWeeklyData.requests
-                }
-              />
-              <Card
-                title={"UNSUBSCRIBED"}
-                data={state.mailWeeklyData.unsubscribes}
-                per={
-                  state.mailWeeklyData.unsubscribes /
-                  state.mailWeeklyData.requests
-                }
-              />
-            </div>
-          ) : (
-            <>
-              {state.mailWeeklyData === false ? (
-                <div>No Data</div>
-              ) : (
-                <div className="main-spinner">
-                  <Loader type="Oval" color="#1B9CFC" height="70" width="70" />
-                </div>
-              )}
-            </>
-          )} */}
         </div>
 
         <div className="mail-status-child">
@@ -276,31 +249,56 @@ const MainPage = props => {
 
               <div className="mail-status-card">
                 <ACard title="OPENED" bordered={false}>
-                  <p>{state.mailMonthlyData.opens}</p>
+                  <p>
+                    {dataPerCalc(
+                      state.mailMonthlyData.opens,
+                      state.mailMonthlyData.requests,
+                    )}
+                  </p>
                 </ACard>
               </div>
 
               <div className="mail-status-card">
                 <ACard title="CLICKED" bordered={false}>
-                  <p>{state.mailMonthlyData.clicks}</p>
+                  <p>
+                    {dataPerCalc(
+                      state.mailMonthlyData.clicks,
+                      state.mailMonthlyData.requests,
+                    )}
+                  </p>
                 </ACard>
               </div>
 
               <div className="mail-status-card">
                 <ACard title="BOUNCES" bordered={false}>
-                  <p>{state.mailMonthlyData.bounces}</p>
+                  <p>
+                    {dataPerCalc(
+                      state.mailMonthlyData.bounces,
+                      state.mailMonthlyData.requests,
+                    )}
+                  </p>
                 </ACard>
               </div>
 
               <div className="mail-status-card">
                 <ACard title="SPAM REPORTS" bordered={false}>
-                  <p>{state.mailMonthlyData.spam_reports}</p>
+                  <p>
+                    {dataPerCalc(
+                      state.mailMonthlyData.spam_reports,
+                      state.mailMonthlyData.requests,
+                    )}
+                  </p>
                 </ACard>
               </div>
 
               <div className="mail-status-card">
                 <ACard title="UNSUBSCRIBED" bordered={false}>
-                  <p>{state.mailMonthlyData.unsubscribes}</p>
+                  <p>
+                    {dataPerCalc(
+                      state.mailMonthlyData.unsubscribes,
+                      state.mailMonthlyData.requests,
+                    )}
+                  </p>
                 </ACard>
               </div>
             </div>
@@ -315,189 +313,8 @@ const MainPage = props => {
               )}
             </>
           )}
-          {/* {state.mailMonthlyData ? (
-            <div className="mail-data">
-              <Card title={"REQUESTS"} data={state.mailMonthlyData.requests} />
-              <Card
-                title={"DELIVERED"}
-                data={state.mailMonthlyData.delivered}
-                per={
-                  state.mailMonthlyData.delivered /
-                  state.mailMonthlyData.requests
-                }
-              />
-              <Card
-                title={"OPENED"}
-                data={state.mailMonthlyData.opens}
-                per={
-                  state.mailMonthlyData.opens / state.mailMonthlyData.requests
-                }
-              />
-              <Card
-                title={"CLICKED"}
-                data={state.mailMonthlyData.clicks}
-                per={
-                  state.mailMonthlyData.clicks / state.mailMonthlyData.requests
-                }
-              />
-              <Card
-                title={"BOUNCES"}
-                data={state.mailMonthlyData.bounces}
-                per={
-                  state.mailMonthlyData.bounces / state.mailMonthlyData.requests
-                }
-              />
-              <Card
-                title={"SPAM REPORTS"}
-                data={state.mailMonthlyData.spam_reports}
-                per={
-                  state.mailMonthlyData.spam_reports /
-                  state.mailMonthlyData.requests
-                }
-              />
-              <Card
-                title={"UNSUBSCRIBED"}
-                data={state.mailMonthlyData.unsubscribes}
-                per={
-                  state.mailMonthlyData.unsubscribes /
-                  state.mailMonthlyData.requests
-                }
-              />
-            </div>
-          ) : (
-            <>
-              {state.mailMonthlyData === false ? (
-                <div>No Data</div>
-              ) : (
-                <div className="main-spinner">
-                  <Loader type="Oval" color="#1B9CFC" height="70" width="70" />
-                </div>
-              )}
-            </>
-          )} */}
         </div>
       </div>
-
-      {/** ===== ====== */}
-
-      {/* <div className="mail-status">
-        {state.mailWeeklyData !== null && state.mailMonthlyData !== null ? (
-          <>
-            <div className="mail-status-child">
-              <h2>Weekly Mail Data</h2>
-              <div className="mail-data">
-                <Card title={"REQUESTS"} data={state.mailWeeklyData.requests} />
-                <Card
-                  title={"DELIVERED"}
-                  data={state.mailWeeklyData.delivered}
-                  per={
-                    state.mailWeeklyData.delivered /
-                    state.mailWeeklyData.requests
-                  }
-                />
-                <Card
-                  title={"OPEND"}
-                  data={state.mailWeeklyData.opens}
-                  per={
-                    state.mailWeeklyData.opens / state.mailWeeklyData.requests
-                  }
-                />
-                <Card
-                  title={"CLICKED"}
-                  data={state.mailWeeklyData.clicks}
-                  per={
-                    state.mailWeeklyData.clicks / state.mailWeeklyData.requests
-                  }
-                />
-                <Card
-                  title={"BOUNCES"}
-                  data={state.mailWeeklyData.bounces}
-                  per={
-                    state.mailWeeklyData.bounces / state.mailWeeklyData.requests
-                  }
-                />
-                <Card
-                  title={"SPAM REPORTS"}
-                  data={state.mailWeeklyData.spam_reports}
-                  per={
-                    state.mailWeeklyData.spam_reports /
-                    state.mailWeeklyData.requests
-                  }
-                />
-                <Card
-                  title={"UNSUBSCRIBED"}
-                  data={state.mailWeeklyData.unsubscribes}
-                  per={
-                    state.mailWeeklyData.unsubscribes /
-                    state.mailWeeklyData.requests
-                  }
-                />
-              </div>
-            </div>
-
-            <div className="mail-status-child">
-              <h2>Monthly Mail Data</h2>
-              <div className="mail-data">
-                <Card
-                  title={"REQUESTS"}
-                  data={state.mailMonthlyData.requests}
-                />
-                <Card
-                  title={"DELIVERED"}
-                  data={state.mailMonthlyData.delivered}
-                  per={
-                    state.mailMonthlyData.delivered /
-                    state.mailMonthlyData.requests
-                  }
-                />
-                <Card
-                  title={"OPEND"}
-                  data={state.mailMonthlyData.opens}
-                  per={
-                    state.mailMonthlyData.opens / state.mailMonthlyData.requests
-                  }
-                />
-                <Card
-                  title={"CLICKED"}
-                  data={state.mailMonthlyData.clicks}
-                  per={
-                    state.mailMonthlyData.clicks /
-                    state.mailMonthlyData.requests
-                  }
-                />
-                <Card
-                  title={"BOUNCES"}
-                  data={state.mailMonthlyData.bounces}
-                  per={
-                    state.mailMonthlyData.bounces /
-                    state.mailMonthlyData.requests
-                  }
-                />
-                <Card
-                  title={"SPAM REPORTS"}
-                  data={state.mailMonthlyData.spam_reports}
-                  per={
-                    state.mailMonthlyData.spam_reports /
-                    state.mailMonthlyData.requests
-                  }
-                />
-                <Card
-                  title={"UNSUBSCRIBED"}
-                  data={state.mailMonthlyData.unsubscribes}
-                  per={
-                    state.mailMonthlyData.unsubscribes /
-                    state.mailMonthlyData.requests
-                  }
-                />
-              </div>
-            </div>
-          </>
-        ) : (
-          <div className="main-spinner">
-            <Loader type="Oval" color="#1B9CFC" height="100" width="100" />
-          </div>
-        )}
-      </div> */}
     </MainPageWrapper>
   ));
 };
